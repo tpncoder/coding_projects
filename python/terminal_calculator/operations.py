c03702c315ef2b07adfbf9cdec417cbae167cd16
Tejas Protim Neog <92169576+tpncoder@users.noreply.github.com>
2022-06-16 20:10:41 +0530

    Added operations.py to terminal_calculator

---
+++ python/terminal_calculator/operations.py
@@ -0,0 +1,19 @@
+def add(a, b):
+    result = a+b
+    print (result)
+
+def subtract(a, b):
+    result = a-b
+    print (result)
+
+def multiply(a, b):
+    result = a*b
+    print (result)
+
+def divide(a, b):
+    result = a/b
+    print(result)
+
+def exponent(a, b):
+    result = a**b
+    print(result)
