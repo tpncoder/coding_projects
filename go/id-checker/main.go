ed78d24f9c3de74eb566614340c78844546b8c38
Tejas Protim Neog <92169576+tpncoder@users.noreply.github.com>
2022-07-16 22:14:55 +0530

    made id-checker (go) better

--- go/id-checker/main.go
+++ go/id-checker/main.go
@@ -1,44 +1,27 @@
 package main
 
-import(
-	"fmt"
-)
+import "fmt"
 
 var choice string
 
-func checkID(){
+func main() {
+	checkID()
+}
+
+func checkID() {
+	id := [10]string{"James", "Tom", "Tejas"}
+
 	var userID string
 	fmt.Print("Enter your ID: ")
 	fmt.Scanln(&userID)
-	id := []string{"James", "Tom", "Tejas"}
 
-	switch(userID){
-		case "James":
-			fmt.Print("Your ID exists!")
+	for i := 0; i < 10; i++ {
+		if id[i] == userID {
+			fmt.Println("ID exists")
 			break
-		case "Tom":
-			fmt.Print("Your ID exists!")
+		} else {
+			fmt.Println("ID doesn't exist")
 			break
-		case "Tejas":
-			fmt.Print("Your ID exists!")
-			break
-		default:
-			fmt.Print("Your ID doesn't exist!\n")
-			fmt.Println("Would you like to create one?\n (y or n)")
-			fmt.Print(">>>")
-			fmt.Scanln(&choice)
-			switch(choice){
-				case "y":
-					id = append(id, userID)
-					fmt.Println("Your ID has been added")
-					fmt.Printf("The current ID list is " + "%v",id)
-					break
-				case "n":
-					break;
-			}
 		}
 	}
-
-func main(){
-	checkID()
 }
